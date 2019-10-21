f898bc3d5d51c2498583f65a24a04f6c113d1845
tbreklo <brekalo.tvrtko@gmail.com>
2019-10-21 22:44:46 +0200

    initial commit

---
+++ src/orange_mapper.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+
+/**
+ * @brief Main program entry point
+ * 
+ * @param argc number of command line arguments
+ * @param argv command line arguments (options or sources)
+ * 
+ * @return int execution success status 
+ */
+int main(int argc, char* argv[]) {
+
+    return EXIT_SUCCESS;
+}
---
+++ test/orange_mapper_tests.cc
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(DummySuite, DummyDoubleEQ) {
+    ASSERT_DOUBLE_EQ(1.0, 1.0);
+}
