9bee274ff85c608d84eb584f477d9717a84a588c
tbreklo <brekalo.tvrtko@gmail.com>
2019-10-21 22:57:58 +0200

    Fixed GTest dummy example

--- test/orange_mapper_tests.cc
+++ test/orange_mapper_tests.cc
@@ -3,3 +3,11 @@
 TEST(DummySuite, DummyDoubleEQ) {
     ASSERT_DOUBLE_EQ(1.0, 1.0);
 }
+
+/**
+ * @brief Main testing entry point
+ */
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv); 
+    return RUN_ALL_TESTS();
+}
