e6b711141162a1e120d3cb4cc18e8c90e57a7a82
yatsukha <yatsukha.roman@gmail.com>
2019-10-23 16:38:26 +0200

    add .clang-format based on google style

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include "config.hpp"
 
 int main() {
-  ::std::cout << "Version: "
-              << BLUE_MAPPER_VERSION_MAJOR << "."
+  ::std::cout << "Version: " << BLUE_MAPPER_VERSION_MAJOR << "."
               << BLUE_MAPPER_VERSION_MINOR << ::std::endl;
 }
