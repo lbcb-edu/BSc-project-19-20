822bd24f2a93f91410fe450660d6decfea00488b
yatsukha <yatsukha.roman@gmail.com>
2019-10-23 16:09:53 +0200

    add a placeholder program

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "config.hpp"
+
+int main() {
+  ::std::cout << "Version: "
+              << BLUE_MAPPER_VERSION_MAJOR << "."
+              << BLUE_MAPPER_VERSION_MINOR << ::std::endl;
+}
