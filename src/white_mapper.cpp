be14f3c3dd1979a570d845650cd2492e802b42bf
matjuric <matija.juric@hotmail.com>
2019-11-02 11:24:32 +0100

    init submodule and headers

--- src/white_mapper.cpp
+++ src/white_mapper.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "config.h"
 
 int main(void) {
 	std::cout << "Test" << std::endl;
 
+	std::cout << WHITE_MAPPER_VERSION_MAJOR << "." << WHITE_MAPPER_VERSION_MINOR << std::endl;
 	return 0;
 }
