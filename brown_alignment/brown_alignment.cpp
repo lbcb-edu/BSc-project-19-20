8867a547718bab08415cc9200599e3518b84b9de
Filip Wolf <filip.wolf@fer.hr>
2019-11-04 13:24:33 +0100

    add matrix initialization

--- brown_alignment/brown_alignment.cpp
+++ brown_alignment/brown_alignment.cpp
@@ -6,38 +6,107 @@ using namespace std;
 
 namespace brown {
 
+    enum class ParentType { up, left, upLeft, none};
+
     struct matrixCell {
-        int cost;
-        char parent;
+        int value;
+        ParentType parent;
     };
+
+    void init_matrix(const char* query, unsigned int query_length,
+                       const char* target, unsigned int target_length,
+                       AlignmentType type,
+                       int match,
+                       int mismatch,
+                       int gap,
+                       vector<vector<matrixCell>> &matrix) {
+        matrix[0][0].parent = ParentType::none;
+        matrix[0][0].value = 0;
+
+        if (type == AlignmentType::global) {
+            for (int i = 1; i < query_length + 1; i++)
+            {
+                matrix[i][0].value = gap*i;
+                matrix[i][0].parent = ParentType::up;
+            }
+            for (int i = 1; i < target_length + 1; i++)
+            {
+                matrix[i][0].value = gap*i;
+                matrix[i][0].parent = ParentType::left;                
+            }
+            
+            for (int i = 1; i < query_length + 1; i++)
+            {
+                for (int j = 1; j < target_length + 1; j++)
+                {
+                    int matchPrev;
+                    if (query[i-1] == target[i-1]) {
+                        matchPrev = matrix[i-1][j-1].value + match;
+                    } else {
+                        matchPrev = matrix[i-1][j-1].value + mismatch;
+                    }
+                    int insertion = matrix[i][j-1].value + gap;
+                    int deletion = matrix[i-1][j].value + gap;
+
+                    if (matchPrev > insertion) {
+                        if (deletion > matchPrev) {
+                            matrix[i][j].value = deletion;
+                            matrix[i][j].parent = ParentType::up;
+                        } else {
+                            matrix[i][j].value = matchPrev;
+                            matrix[i][j].parent = ParentType::upLeft;
+                        }
+                    } else {
+                        if (deletion > insertion) {
+                            matrix[i][j].value = deletion;
+                            matrix[i][j].parent = ParentType::up;                            
+                        } else {
+                            matrix[i][j].value = insertion;
+                            matrix[i][j].parent = ParentType::left;                            
+                        }
+                    }
+
+                }
+                
+            }
+            
+        }
+    }
+
+
 
     int global_alignment(const char* query, unsigned int query_length,
                        const char* target, unsigned int target_length,
+                       AlignmentType type,
                        int match,
                        int mismatch,
                        int gap) {
 
-        vector<vector<int>> matrix(query_length + 1, vector<int>(target_length + 1, 0));
+        vector<vector<matrixCell>> matrix(query_length + 1, vector<matrixCell>(target_length + 1));
+
+
 
     }
 
     int local_alignment(const char* query, unsigned int query_length,
                        const char* target, unsigned int target_length,
+                       AlignmentType type,
                        int match,
                        int mismatch,
                        int gap) {
 
-        vector<vector<int>> matrix(query_length + 1, vector<int>(target_length + 1, 0));
+        vector<vector<matrixCell>> matrix(query_length + 1, vector<matrixCell>(target_length + 1));
 
     }
 
     int semi_global_alignment(const char* query, unsigned int query_length,
                        const char* target, unsigned int target_length,
+                       AlignmentType type,
                        int match,
                        int mismatch,
                        int gap) {
 
-        vector<vector<int>> matrix(query_length + 1, vector<int>(target_length + 1, 0));
+        vector<vector<matrixCell>> matrix(query_length + 1, vector<matrixCell>(target_length + 1));
     }    
 
     int pairwise_alignment(const char* query, unsigned int query_length,
@@ -50,11 +119,11 @@ namespace brown {
         switch (type)
         {
         case AlignmentType::global:
-            return global_alignment(query, query_length, target, target_length, match, mismatch, gap);
+            return global_alignment(query, query_length, target, target_length, type, match, mismatch, gap);
         case AlignmentType::local:
-            return local_alignment(query, query_length, target, target_length, match, mismatch, gap);
+            return local_alignment(query, query_length, target, target_length, type, match, mismatch, gap);
         case  AlignmentType::semi_global:
-            return semi_global_alignment(query, query_length, target, target_length, match, mismatch, gap);
+            return semi_global_alignment(query, query_length, target, target_length, type, match, mismatch, gap);
         default:
             fprintf(stderr, "Unknown alignment type\n");
             break;
