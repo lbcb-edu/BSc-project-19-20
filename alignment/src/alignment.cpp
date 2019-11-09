81ecbfb837873c278bd2cea0b39bee25afbbdb03
yatsukha <yatsukha.roman@gmail.com>
2019-11-09 13:15:26 +0100

    minor refactoring

--- alignment/src/alignment.cpp
+++ alignment/src/alignment.cpp
@@ -1,17 +1,27 @@
 #include "alignment/alignment.hpp"
 
-// TODO: delet
-#include <iostream>
-
 namespace algn {
 namespace detail {
 
-enum Action { kMatch, kMismatch, kInsert, kDelete, kNone };
+// clang-format off
+enum Action {
+  kMatch    = '=',
+  kMismatch = 'X',
+  kInsert   = 'D',
+  kDelete   = 'I',
+  kNone     = '?'
+};
+// clang-format on
 
 struct Cell {
-  int score;
+  int score = 0;
   Action action = kNone;
 };
+
+inline Cell max(Cell a, Cell b, Cell c) {
+  return a.score > b.score ? a.score > c.score ? a : c
+                           : b.score > c.score ? b : c;
+}
 
 Contiguous2DArray<Cell> NeedlemanWunsch(const char* query,
                                         unsigned int query_length,
@@ -20,7 +30,7 @@ Contiguous2DArray<Cell> NeedlemanWunsch(const char* query,
                                         int mismatch, int gap) {
   Contiguous2DArray<Cell> alignment(query_length + 1, target_length + 1);
 
-  for (int j = 0; j < alignment.cols; ++j)
+  for (int j = 1; j < alignment.cols; ++j)
     alignment[0][j] = {j * gap, Action::kInsert};
 
   for (int i = 1; i < alignment.rows; ++i) {
@@ -28,65 +38,53 @@ Contiguous2DArray<Cell> NeedlemanWunsch(const char* query,
 
     for (int j = 1; j < alignment.cols; ++j) {
       bool is_match = query[i - 1] == target[j - 1];
-      int aligned =
-              alignment[i - 1][j - 1].score + (is_match ? match : mismatch),
-          inserted = alignment[i][j - 1].score + gap,
-          deleted = alignment[i - 1][j].score + gap;
-
-      // clang-format off
-      if (aligned > inserted)
-        if (aligned > deleted)
-          alignment[i][j] = {aligned,
-                             is_match ? Action::kMatch : Action::kMismatch};
-        else
-          alignment[i][j] = {deleted,  Action::kDelete};
-      else 
-        if (inserted > deleted)
-          alignment[i][j] = {inserted, Action::kInsert};
-      else
-          alignment[i][j] = {deleted,  Action::kDelete};
-      // clang-format on
+      alignment[i][j] =
+          max({alignment[i - 1][j - 1].score + (is_match ? match : mismatch),
+               is_match ? Action::kMatch : Action::kMismatch},
+              {alignment[i][j - 1].score + gap, Action::kInsert},
+              {alignment[i - 1][j].score + gap, Action::kDelete});
     }
   }
 
   return alignment;
 }
 
+using Position = ::std::pair<::std::size_t, ::std::size_t>;
+
+inline void UpdatePosition(Action action, Position& pos) {
+  switch (action) {
+    case Action::kMatch:
+      --pos.first;
+      --pos.second;
+      break;
+
+    case Action::kMismatch:
+      --pos.first;
+      --pos.second;
+      break;
+
+    case Action::kInsert:
+      --pos.second;
+      break;
+
+    case Action::kDelete:
+      --pos.first;
+      break;
+  }
+}
+
 void ConstructCigar(const detail::Contiguous2DArray<detail::Cell>& alignment,
                     ::std::string& cigar, unsigned int& target_begin) {
   detail::Cell current = alignment.last();
-  ::std::pair<::std::size_t, ::std::size_t> pos{alignment.rows - 1,
-                                                alignment.cols - 1};
+  Position pos{alignment.rows - 1, alignment.cols - 1};
 
   bool begin_set = false;
 
-  while (true) {
-    switch (current.action) {
-      case Action::kMatch:
-        cigar.append("=");
-        --pos.first;
-        --pos.second;
-        break;
-
-      case Action::kMismatch:
-        cigar.append("X");
-        --pos.first;
-        --pos.second;
-        break;
-
-      case Action::kInsert:
-        cigar.append("D");
-        --pos.second;
-        break;
-
-      case Action::kDelete:
-        cigar.append("I");
-        --pos.first;
-        break;
-    }
+  while (current.action != kNone) {
+    cigar += current.action;
+    UpdatePosition(current.action, pos);
 
     if (!pos.first && !begin_set) target_begin = pos.second;
-    if (pos == decltype(pos){0, 0}) break;
 
     current = alignment[pos.first][pos.second];
   }
