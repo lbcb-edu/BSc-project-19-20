d9861e3736d876fcbc4c22dd099dc3f9470e96a9
matjuric <matija.juric@hotmail.com>
2019-11-09 12:02:56 +0100

    restructuring

--- src/white_mapper.cpp
+++ src/white_mapper.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <algorithm>
 #include "config.h"
-#include "options.h"
+#include "options.hpp"
 #include <bioparser/bioparser.hpp>
 
 
@@ -47,12 +47,7 @@ class FASTQformat {
 
 
 int main(int argc, char* argv[]) {
-	/*
-	std::cout << "Test" << std::endl;
 	
-	std::cout << argc << std::endl;
-	*/
-
 	switch (argc) {
 		case 1:
 			noArgumentsPassed();
@@ -79,28 +74,28 @@ int main(int argc, char* argv[]) {
 			}
 			std::string file_extension = first_file.substr(it + 1, first_file.size() - it);
 			//std::cout << file_extension << std::endl;
+			
+			std::vector<std::unique_ptr<FASTAformat>> fasta_objects;
+			auto fasta_parser = bioparser::createParser<bioparser::FastaParser, FASTAformat>(argv[1]);
+			fasta_parser->parse(fasta_objects, -1);
+			
+			int mini = 999999;
+			int maxi = 0;
+			int sum = 0;
+			for (int i = 0; i < fasta_objects.size(); i++) {
+				mini = std::min(mini, fasta_objects[i]->sequence_length);
+				maxi = std::max(maxi, fasta_objects[i]->sequence_length);
+				sum += fasta_objects[i]->sequence_length;
+			}
+
+			double avg = (double)sum / fasta_objects.size();
+
+			std::cout << "Number of sequences: " << fasta_objects.size() << std::endl;
+			std::cout << "Average length of sequence: " << avg << std::endl;
+			std::cout << "Minimal length of sequence: " << mini << std::endl;
+			std::cout << "Maximal length of sequence: " << maxi << std::endl;
 			break;
 	}
-
-	std::vector<std::unique_ptr<FASTAformat>> fasta_objects;
-	auto fasta_parser = bioparser::createParser<bioparser::FastaParser, FASTAformat> (argv[1]);
-	fasta_parser->parse(fasta_objects, -1);
-
-	int mini = 999999;
-	int maxi = 0;
-	int sum = 0;
-	for (int i = 0; i < fasta_objects.size(); i++) {
-		mini = std::min(mini, fasta_objects[i]->sequence_length);
-		maxi = std::max(maxi, fasta_objects[i]->sequence_length);
-		sum += fasta_objects[i]->sequence_length;
-	}
-	
-	double avg = (double)sum / fasta_objects.size();
-	
-	std::cout << "Number of sequences: " << fasta_objects.size() << std::endl;
-	std::cout << "Average length of sequence: " << avg << std::endl;
-	std::cout << "Minimal length of sequence: " << mini << std::endl;
-	std::cout << "Maximal length of sequence: " << maxi << std::endl;
 
 	return 0;
 }
