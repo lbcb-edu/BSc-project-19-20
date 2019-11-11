77cf1bab7867a5a52e1cdbc32247cc201fc22836
tbreklo <brekalo.tvrtko@gmail.com>
2019-11-11 21:56:30 +0100

    'Hasta la vista, baby' 'Au Revoir, mon amour'

--- src/orange_alignment.cc
+++ src/orange_alignment.cc
@@ -12,86 +12,79 @@ namespace orange {
 namespace alignment {
 
 /**
+ * @brief Estimates CIGAR string lenght based on query length
+ */
+auto constexpr kAproxLenFactor = 0.8;
+
+/**
  * @brief Describes the location of the parent
  *      cell relative to queried cell position
  */
 enum ParentDir {
+    kStop,  //<<< Marks an undefined score origin
+
     kDiagonal,  //<<< Marks a diagonal move in the matrix
     kLeft,      //<<< Marks a move from the left in the matrix
     kUp,        //<<< Marks a move from above in the matrix
-
-    kNo  //<<< The cell is located at the left or upper edge of the matrix
 };
-
-/**
- * @brief Represents score in the scoring matrix
- *      for sequence alignment algorithms without backtracking for CIGAR
- */
-using Score = int;
 
 /**
  * @brief Cell in the scoring matrix
  *      for sequence aligment mamorizing parent cell relative position
  */
-struct MemCell {
-    Score score_;
+struct Cell {
+    int score_;
     ParentDir parent_;  //<<< marks the origin of the scoring
+
+    bool operator<(const Cell& other) const { return score_ < other.score_; }
 };
 
 /**
- * @brief References surrounding cells
- *      relevant for updating the score
+ * @brief Holds coordinates of a cell in a matrix
  */
-struct CellSurr {
-    MemCell& diag_;
-    MemCell& left_;
-    MemCell& up_;
+struct Pos {
+    unsigned int r_;  //<<< Row coordinate
+    unsigned int c_;  //<<< Column coordinate
 };
-
-template <typename Cell>
-using TRow = std::vector<Cell>;
-
-using Matrix = std::vector<TRow<MemCell>>;
-
-template <typename Row>
-auto gapRowFactory(int gap, int sz) {
-    auto row = Row(sz + 1);
-    for (auto i = int{1}; i <= sz; ++i) row[i] = 1 * gap;
-    return row;
-}
 
 int pairwiseAlignment(char const* query, unsigned int query_length,
                       char const* target, unsigned int target_length,
                       AlignmentType type, int match, int mismatch, int gap) {
-    using Row = TRow<Score>;
-    using ScoreFn_t = std::function<int(Row&, Row&, const int&, int)>;
+    using Row = std::vector<int>;
+    using ScoreFn = std::function<int(Row&, Row&, const int&, const int)>;
 
-    auto mx_val = std::numeric_limits<int>::min();
+    auto mx_cell = std::numeric_limits<int>::min();
     auto rows = [&type, &gap, &target_length] {
+        auto rows = std::array<Row, 2>{Row(target_length + 1, {0}),
+                                       Row(target_length + 1, {0})};
+
         if (type == AlignmentType::kGlobal)
-            return std::array<Row, 2>{gapRowFactory<Row>(gap, target_length),
-                                      Row(target_length + 1, {0})};
-        return std::array<Row, 2>{Row(target_length + 1, {0}),
-                                  Row(target_length + 1, {0})};
+            for (auto i = int{1}; i <= target_length; ++i) rows[0][i] = i * gap;
+
+        return rows;
     }();
 
-    auto scoreFn = [&type, &mx_val, &gap]() -> ScoreFn_t {
+    /**
+     * @brief determines scoring function based on alignment type
+     */
+    auto scoreFn = [&type, &mx_cell, &gap]() -> ScoreFn {
         if (type == AlignmentType::kLocal)
-            return [&mx_val, &gap](Row& prev, Row& curr, const int& pos,
-                                   int matched) {
+            return [&mx_cell, &gap](Row& prev, Row& curr, const int& pos,
+                                    const int matched) {
                 auto ret = std::max({prev[pos - 1] + matched,
                                      curr[pos - 1] + gap, prev[pos] + gap, 0});
-                mx_val = std::max(mx_val, ret);
+                mx_cell = std::max(mx_cell, ret);
 
                 return ret;
             };
 
-        return [&gap](Row& prev, Row& curr, const int& pos, int matched) {
+        return [&gap](Row& prev, Row& curr, const int& pos, const int matched) {
             return std::max({prev[pos - 1] + matched, curr[pos - 1] + gap,
                              prev[pos] + gap});
         };
     }();
 
+    // Matrix traversal
     for (auto i = int{1}; i <= query_length; ++i) {
         auto& prev = rows[(i - 1) & 1];
         auto& curr = rows[i & 1];
@@ -101,15 +94,14 @@ int pairwiseAlignment(char const* query, unsigned int query_length,
 
         for (auto j = int{1}; j <= target_length; ++j) {
             curr[j] = scoreFn(prev, curr, j,
-                              (query[i] == target[j]) ? match : mismatch);
+                              (query[i - 1] == target[j - 1]) ? match : mismatch);
         }
     }
 
+    const auto& last_row = rows[query_length & 1];
     if (type == AlignmentType::kLocal)
-        return mx_val;
+        return mx_cell;
     else {
-        auto& last_row = rows[query_length & 1];
-
         if (type == AlignmentType::kGlobal)
             return last_row.back();
 
@@ -122,9 +114,139 @@ int pairwiseAlignment(char const* query, unsigned int query_length,
                       char const* target, unsigned int target_length,
                       AlignmentType type, int match, int mismatch, int gap,
                       std::string& cigar, unsigned int& target_begin) {
-    using Row = TRow<MemCell>;
+    using Row = std::vector<Cell>;
+    using Matrix = std::vector<Row>;
+    using ScoreFn = std::function<Cell(const int&, const int&, const int)>;
 
-    return 0;
+    using namespace std::string_literals;
+
+    auto matrix = [&type, &query_length, &target_length, &gap]() -> Matrix {
+        auto matrix = Matrix(query_length + 1,
+                             Row(target_length + 1, {0, ParentDir::kStop}));
+
+        if (type == AlignmentType::kGlobal)
+            for (auto i = int{1}; i <= target_length; ++i)
+                matrix[0][i].score_ = i * gap;
+
+        if (type != AlignmentType::kLocal)
+            for (auto i = int{1}; i <= query_length; ++i)
+                matrix[i][0].score_ = i * gap;
+
+        return matrix;
+    }();
+
+    auto mx_cell = Cell{std::numeric_limits<int>::min(), ParentDir::kStop};
+    auto mx_pos = Pos{0, 0};
+
+    auto scoreGlobFn = [&matrix, &gap](const int& r, const int& c,
+                                       const int matched) {
+        return std::max({
+            Cell{matrix[r - 1][c - 1].score_ + matched, ParentDir::kDiagonal},
+            Cell{matrix[r][c - 1].score_ + gap, ParentDir::kLeft},
+            Cell{matrix[r - 1][c].score_ + gap, ParentDir::kUp},
+        });
+    };
+
+    auto scoreFn = [&type, &mx_cell, &mx_pos, &scoreGlobFn]() -> ScoreFn {
+        if (type == AlignmentType::kLocal) {
+            return [&mx_cell, &mx_pos, &scoreGlobFn](const unsigned int& r,
+                                                     const unsigned int& c,
+                                                     const int matched) {
+                auto ret = std::max(scoreGlobFn(r, c, matched),
+                                    Cell{0, ParentDir::kStop});
+
+                if (mx_cell < ret) {
+                    mx_cell = ret;
+                    mx_pos = {r, c};
+                }
+
+                return ret;
+            };
+        } else
+            return scoreGlobFn;
+    }();
+
+    for (auto i = std::uint32_t{1}; i <= query_length; ++i) {
+        for (auto j = std::uint32_t{1}; j <= target_length; ++j) {
+            matrix[i][j] =
+                scoreFn(i, j, (query[i - 1] == target[j - 1]) ? match : mismatch);
+        }
+    }
+
+    auto backtrack = [&matrix, &cigar, &target_begin](Pos pos) {
+        auto nextPos = [](const Pos& curr, const ParentDir& dir) {
+            switch (dir) {
+                case ParentDir::kDiagonal:
+                    return Pos{curr.r_ - 1, curr.c_ - 1};
+                case ParentDir::kLeft:
+                    return Pos{curr.r_, curr.c_ - 1};
+                case ParentDir::kUp:
+                    return Pos{curr.r_ - 1, curr.c_};
+                default:
+                    throw std::logic_error("Ups, something went wrong. :(");
+            }
+        };
+
+        auto cigarFrag = [](ParentDir dir, int len) {
+            auto n = std::to_string(len);
+            switch (dir) {
+                case ParentDir::kDiagonal:
+                    return "M" + n;
+                case ParentDir::kLeft:
+                    return "D" + n;
+                case ParentDir::kUp:
+                    return "I" + n;
+
+                default:
+                    throw std::logic_error("Ups, something went wrong. :(");
+            }
+
+            return ""s;
+        };
+        cigar.reserve(matrix.size() * kAproxLenFactor);
+
+        auto curr_dir = matrix[pos.r_][pos.c_].parent_;
+        auto curr_pos = pos;
+        int cnt = 1;
+
+        while (curr_dir != kStop) {
+            auto nxt_pos = nextPos(curr_pos, curr_dir);
+            const auto& nxt_dir = matrix[nxt_pos.r_][nxt_pos.c_].parent_;
+
+            if (nxt_dir != curr_dir) {
+                cigar += cigarFrag(curr_dir, cnt);
+
+                curr_dir = nxt_dir;
+                cnt = 1;
+            } else
+                ++cnt;
+
+            curr_pos = nxt_pos;
+        }
+
+        std::reverse(begin(cigar), end(cigar));
+        target_begin = curr_pos.c_;
+    };
+
+    switch (type) {
+        case AlignmentType::kLocal:
+            backtrack({mx_pos.r_, mx_pos.c_});
+            return mx_cell.score_;
+
+        case AlignmentType::kGlobal:
+            backtrack({query_length, target_length});
+            return matrix[query_length][target_length].score_;
+
+        case AlignmentType::kSemiGlobal:
+            auto& last_row = matrix[query_length];
+
+            auto pos = static_cast<unsigned int>(
+                std::max_element(begin(last_row), end(last_row)) -
+                last_row.begin());
+
+            backtrack({query_length, pos});
+            return last_row[pos].score_;
+    }
 }
 
 }  // namespace alignment
--- test/ornage_alignment_test.cc
+++ test/ornage_alignment_test.cc
@@ -2,7 +2,6 @@
 
 #include "orange_alignment.h"
 
-
 TEST(PairwiseAlignmentRow, global) {
     EXPECT_EQ(orange::alignment::pairwiseAlignment(
                   "GCATGCU", 7, "GATTACA", 7,
@@ -21,5 +20,29 @@ TEST(PairwiseAlignmentRow, semi_global) {
     EXPECT_EQ(orange::alignment::pairwiseAlignment(
                   "TCCG", 4, "ACTCCGAT", 8,
                   orange::alignment::AlignmentType::kSemiGlobal, 4, -1, -2),
-              12);
+              16);
+}
+
+TEST(PairwiseAlignmentMatrix, global) {
+    std::string cigar;
+    unsigned int target_start = 0;
+
+    EXPECT_EQ(orange::alignment::pairwiseAlignment(
+                  "GCATGCU", 7, "GATTACA", 7,
+                  orange::alignment::AlignmentType::kGlobal, 1, -1, -1, cigar,
+                  target_start),
+              0);
+    EXPECT_EQ(target_start, 0);
+}
+
+TEST(PairwiseAlignmentMatrix, local) {
+    std::string cigar;
+    unsigned int target_start = 0;
+
+    EXPECT_EQ(orange::alignment::pairwiseAlignment(
+                  "ACCTAAGG", 8, "GGCTCAATCA", 10,
+                  orange::alignment::AlignmentType::kLocal, 2, -1, -2, cigar,
+                  target_start),
+              6);
+    EXPECT_EQ(target_start, 2);
 }
