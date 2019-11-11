b769c9372494309d75a6afcf540c8d94dc9c1bfd
tumpeccet <nera143@gmail.com>
2019-11-12 00:55:57 +0100

    Add setup for alignment library

---
+++ white_alignment/white_alignment.hpp
@@ -0,0 +1,23 @@
+namespace white {
+
+    enum AlignmentType {
+        kGlobal,
+        kLocal,
+        kSemiGlobal};
+
+    int PairwiseAlignment(const char* query, unsigned int query_length,
+                           const char* target, unsigned int target_length,
+                           AlignmentType type,
+                           int match,
+                           int mismatch,
+                           int gap);
+
+    int PairwiseAlignment(const char* query, unsigned int query_length,
+                           const char* target, unsigned int target_length,
+                           AlignmentType type,
+                           int match,
+                           int mismatch,
+                           int gap,
+                           std::string& cigar,
+                           unsigned int& target_begin);
+}
