bf312348c58321e2f1e90a259d3c1b4c0b24b668
yatsukha <yatsukha.roman@gmail.com>
2019-11-13 17:56:06 +0100

    add strong types to function prototypes for better call site type safety

--- alignment/test/alignment.t.cpp
+++ alignment/test/alignment.t.cpp
@@ -30,15 +30,17 @@ TEST(ContiguousArrayTest, IndexingTest) {
     for (auto j = 0; j < c_arr.cols; ++j) ASSERT_EQ(i * j, c_arr[i][j]);
 }
 
+// examples taken from
+// https://www.fer.unizg.hr/_download/repository/bioinformatika_skripta_v1.2.pdf#page=24
+
 TEST(NeedleWunschTest, GeneralTest) {
   ::std::string cigar;
   unsigned int target_begin;
 
-  // example taken from
-  // https://www.fer.unizg.hr/_download/repository/bioinformatika_skripta_v1.2.pdf#page=24
-  auto score = PairwiseAlignment("TGCATAT", 7, "ATCCGAT", 7,
-                                 AlignmentType::kNeedlemanWunsch, 2, -1, -1,
-                                 cigar, target_begin);
+  auto score =
+      PairwiseAlignment(Query{"TGCATAT"}, QueryLength{7}, Target{"ATCCGAT"},
+                        TargetLength{7}, AlignmentType::kNeedlemanWunsch,
+                        Match{2}, Mismatch{-1}, Gap{-1}, cigar, target_begin);
 
   ASSERT_EQ(0, target_begin);
   ASSERT_EQ("D=X=XI==", cigar);
@@ -49,9 +51,10 @@ TEST(SmithWatermanTest, GeneralTest) {
   ::std::string cigar;
   unsigned int target_begin;
 
-  auto score = PairwiseAlignment("ACCTAAGG", 8, "GGCTCAATCA", 10,
-                                 AlignmentType::kSmithWaterman, 2, -1, -2,
-                                 cigar, target_begin);
+  auto score =
+      PairwiseAlignment(Query{"ACCTAAGG"}, QueryLength{8}, Target{"GGCTCAATCA"},
+                        TargetLength{10}, AlignmentType::kSmithWaterman,
+                        Match{2}, Mismatch{-1}, Gap{-2}, cigar, target_begin);
 
   ASSERT_EQ(2, target_begin);
   ASSERT_EQ(6, score);
