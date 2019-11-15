41172694b9e23f6ef95e3b7f368914002fb10a90
yatsukha <yatsukha.roman@gmail.com>
2019-11-15 19:53:27 +0100

    add random sequence alignment to main.cpp; fix a character termination bug

--- include/sequence.hpp
+++ include/sequence.hpp
@@ -7,16 +7,16 @@ namespace mapper {
 
 struct Sequence {
   ::std::string name, sequence;
-  Sequence(const char* name, ::std::uint32_t, const char* sequence,
-           ::std::uint32_t)
-      : name(name), sequence(name) {}
+  Sequence(const char* name, ::std::uint32_t name_len, const char* sequence,
+           ::std::uint32_t seq_len)
+      : name(name, name_len), sequence(sequence, seq_len) {}
 };
 
 struct QSequence : Sequence {
   ::std::string quality;
-  QSequence(const char* name, ::std::uint32_t i1, const char* sequence,
-            ::std::uint32_t i2, const char* quality, ::std::uint32_t)
-      : Sequence(name, i1, sequence, i2), quality(quality) {}
+  QSequence(const char* name, ::std::uint32_t name_len, const char* sequence,
+            ::std::uint32_t seq_len, const char* quality, ::std::uint32_t q_len)
+      : Sequence(name, name_len, sequence, seq_len), quality(quality, q_len) {}
 };
 
 }  // namespace mapper
