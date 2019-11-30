03acf03cf551909c04988a95582f669ac196b74e
ivona.mart <ivona.martinovic@fer.hr>
2019-11-30 22:06:41 +0100

    test modified

--- test/pink_mapper_test.cpp
+++ test/pink_mapper_test.cpp
@@ -11,13 +11,13 @@ TEST(PairwiseAlignment, global) {
 TEST(PairwiseAlignment, semi_global) {
     std::string cigar;
     int target_begin;
-    EXPECT_EQ (pink::pairwise_alignment ("ACCTAAGG", 8, "GGCTCAATCA", 10, orange::AlignmentType::local, 2, -1, -2, cigar, target_start), 6);
+    EXPECT_EQ (pink::pairwise_alignment ("ACCCAAGGG", 9, "GGCTCCATTA", 10, pink::semi_global, 1, -1, -1, cigar, target_start), 1);
     EXPECT_EQ (target_start, 2);
 }
 
 TEST(PairwiseAlignment, local) {
     std::string cigar;
     unsigned int target_begin = 0;
-    EXPECT_EQ(6, blue::pairwise_alignment("ACCTAAGG", 8, "GGCTCAATCA", 10, blue::local, 2, -1, -2, cigar, target_begin));
+    EXPECT_EQ(6, pink::pairwise_alignment("ACCTAAGG", 8, "GGCTCAATCA", 10, pink::local, 2, -1, -2, cigar, target_begin));
     EXPECT_EQ(target_begin, 2);
 }
