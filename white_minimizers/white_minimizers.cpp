415d7de02018bde64441fb39137784ef72074ff3
tumpeccet <nera143@gmail.com>
2019-12-13 13:32:22 +0100

    Add minimizers setup

---
+++ white_minimizers/white_minimizers.cpp
@@ -0,0 +1,12 @@
+#include "white_minimizers.hpp"
+#include <vector>
+
+namespace white{
+    std::vector<std::tuple<unsigned int, unsigned int, bool>> minimizers(const char* sequence, unsigned int sequence_length,
+                                                                         unsigned int k,
+                                                                         unsigned int window_length){
+
+
+    }
+}
+
---
+++ white_minimizers/white_minimizers.hpp
@@ -0,0 +1,7 @@
+#include <vector>
+
+namespace white{
+    std::vector<std::tuple<unsigned int, unsigned int, bool>> minimizers(const char* sequence, unsigned int sequence_length,
+                                                                         unsigned int k,
+                                                                         unsigned int window_length);
+}
