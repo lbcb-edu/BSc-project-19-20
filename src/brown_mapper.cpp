bad6d0b1955804c4a2ed16034688bfafc00e8e36
Filip Wolf <filip.wolf@fer.hr>
2020-01-09 14:54:15 +0100

    add LIS algorithm

--- src/brown_mapper.cpp
+++ src/brown_mapper.cpp
@@ -113,6 +113,62 @@ string convertToString(char *a, int size) {
   return s;
 }
 
+// Binary search 
+int GetCeilIndex(int arr[], vector<int>& T, int l, int r, 
+                 int key) 
+{ 
+    while (r - l > 1) { 
+        int m = l + (r - l) / 2; 
+        if (arr[T[m]] >= key) 
+            r = m; 
+        else
+            l = m; 
+    } 
+  
+    return r; 
+} 
+
+//LIS algorithm (nlogn)
+//https://www.geeksforgeeks.org/construction-of-longest-monotonically-increasing-subsequence-n-log-n/
+int LongestIncreasingSubsequence(vector<pair<int, int>> matches, int n) 
+{ 
+    // Add boundary case, when array n is zero
+    // Depend on smart pointers
+  
+    vector<int> tailIndices(n, 0); // Initialized with 0
+    vector<int> prevIndices(n, -1); // initialized with -1
+  
+    int len = 1; // it will always point to empty location
+    for (int i = 1; i < n; i++) {
+        if (matches[i].second < matches[tailIndices[0]].second) {
+            // new smallest value 
+            tailIndices[0] = i; 
+        } 
+        else if (matches[i].second > matches[tailIndices[len - 1]].second) { 
+            // arr[i] wants to extend largest subsequence 
+            prevIndices[i] = tailIndices[len - 1]; 
+            tailIndices[len++] = i; 
+        } 
+        else { 
+            // arr[i] wants to be a potential condidate of 
+            // future subsequence 
+            // It will replace ceil value in tailIndices 
+            // int pos = GetCeilIndex(arr, tailIndices, -1, 
+            //                        len - 1, arr[i]); 
+  
+            // prevIndices[i] = tailIndices[pos - 1]; 
+            // tailIndices[pos] = i; 
+        } 
+    } 
+  
+    // cout << "LIS of given input" << endl; 
+    // for (int i = tailIndices[len - 1]; i >= 0; i = prevIndices[i]) 
+    //     cout << arr[i] << " "; 
+    // cout << endl; 
+  
+    // return len; 
+} 
+
 int main(int argc, char **argv) {
   bool flag = false;
   int match = 0, mismatch = 0, gap = 0, k = 15, w = 5;
@@ -215,6 +271,7 @@ int main(int argc, char **argv) {
   }
 
   // parse 1st file as FASTA
+  // oWo
   if (has_suffix(convertToString(argv[13], strlen(argv[13])), ".fasta") ||
       has_suffix(convertToString(argv[13], strlen(argv[13])), ".fa") ||
       has_suffix(convertToString(argv[13], strlen(argv[13])), ".fasta.gz") ||
@@ -279,10 +336,20 @@ int main(int argc, char **argv) {
             "most frequent: "
          << kul[f].second << "\n";
     cout << "\n";
-
+    vector<pair<int, int>> matches;
+    for (int i = 0; i < minimizersListRef.size(); i++)
+    {
+      for (int j = 0; j < minimizersList.size(); j++)
+      {
+        if (get<0>(minimizersListRef[i]) == get<0>(minimizersList[j])) matches.push_back(make_pair(i, j));
+      }
+    }  
+    LongestIncreasingSubsequence(matches, matches.size());
   }
 
   // parse 1st file as FASTQ
+  // :3
+  // <3
   else if (has_suffix(convertToString(argv[13], strlen(argv[13])), ".fastq") ||
            has_suffix(convertToString(argv[13], strlen(argv[13])), ".fq") ||
            has_suffix(convertToString(argv[13], strlen(argv[13])),
