8cf45ecf2e27a89ef27503b9ec68be4df36ebabb
tumpeccet <nera143@gmail.com>
2020-01-20 15:53:47 +0100

    Minimizers test setup

---
+++ tests/minimizers_test.cpp
@@ -0,0 +1,26 @@
+//
+// Created by nera on 16. 01. 2020..
+//
+#include <gtest/gtest.h>
+#include "../white_minimizers/white_minimizers.cpp"
+
+//TEST(minimizers, test1) {
+//
+//}
+//
+//TEST(minimizers, test2) {
+//
+//}
+//
+//TEST(minimizers, test3) {
+//
+//}
+//
+//TEST(minimizers, test4) {
+//
+//}
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
